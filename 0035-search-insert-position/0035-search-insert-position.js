8d24e33928a50e868144fb8a07723e6c0ce1a439
Mohammed Jisam <66182731+Mohammedjisam@users.noreply.github.com>
2024-10-24 17:48:24 +0530

    Time: 0 ms (100%), Space: 49.3 MB (23.1%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -0,0 +1,22 @@
+
+    /**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    let value;
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]===target){
+            value=i;
+            break;
+        }else if(nums[i]<=target){
+            value=i+1;
+        }
+        else if(nums[0]>=target){
+            value=i;
+            break;
+        }
+    }return value;
+    
+};
