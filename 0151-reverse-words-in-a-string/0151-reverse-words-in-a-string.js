fe4c3d9dc549c6f27b1544a6a02121fc6d57564b
Mohammed Jisam <66182731+Mohammedjisam@users.noreply.github.com>
2024-12-19 09:59:57 +0530

    Time: 5 ms (21.69%), Space: 52.2 MB (9.1%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+    const splitS=s.split(" ");
+    const stack=[]
+    for(let i of splitS){
+        stack.push(i)
+    }
+    let finalS="";
+    while(stack.length){
+        const current=stack.pop();
+
+        if(current){
+            finalS+=" "+current;
+        }
+    }
+    return finalS.trim();
+
+}
