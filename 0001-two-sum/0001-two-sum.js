1d106bfd40940ff6319969f937a7ca72aff8f18c
Mohammed Jisam <66182731+Mohammedjisam@users.noreply.github.com>
2024-12-26 19:13:04 +0530

    Time: 1 ms (89.6%), Space: 51.1 MB (43.08%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,16 +4,13 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    const numsObj = {};
-    nums.forEach((num, index) => {
-        numsObj[num.toString()] = index;
-    });
-
-    // Find the two indices
-    for (let i = 0; i < nums.length; i++) {
-        const possibleIndex = numsObj[(target - nums[i]).toString()];
-        if (possibleIndex !== undefined && possibleIndex !== i) {
-            return [i, possibleIndex];
+    const hashTable={}
+    for(let i=0;i<nums.length;i++){
+        const complement=target-nums[i]
+        if(hashTable.hasOwnProperty(complement)){
+            return [hashTable[complement],i]
         }
+        hashTable[nums[i]]=i;
     }
+    return [];
 };
