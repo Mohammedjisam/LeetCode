21dad6428f566e79aa64470ed0aa42cd0edf97e4
Mohammed Jisam <66182731+Mohammedjisam@users.noreply.github.com>
2024-12-26 19:32:43 +0530

    Time: 1 ms (89.6%), Space: 50.3 MB (58.85%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,13 +4,13 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    const hashTable={}
-    for(let i=0;i<nums.length;i++){
-        const complement=target-nums[i]
-        if(hashTable.hasOwnProperty(complement)){
-            return [hashTable[complement],i]
+    const hashTable = {};
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+        if (hashTable.hasOwnProperty(complement)) {
+            return [hashTable[complement], i];
         }
-        hashTable[nums[i]]=i;
+        hashTable[nums[i]] = i;
     }
-    return [];
+ return [];
 };
