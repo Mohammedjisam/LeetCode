c81582ddd238d7d5a9a02f14219601f838da2ae0
Mohammed Jisam <66182731+Mohammedjisam@users.noreply.github.com>
2025-01-09 13:07:26 +0530

    Time: 40 ms (61.63%), Space: 73.3 MB (33.18%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
@@ -3,20 +3,17 @@
  * @return {number}
  */
 var longestConsecutive = function(nums) {
-    if (nums.length === 0) return 0;
-    let numSet = new Set(nums); 
-    let longestStreak = 0;
-    for (let num of numSet) {
-        if (!numSet.has(num - 1)) {
-            let currentNum = num;
-            let currentStreak = 1;
-            while (numSet.has(currentNum + 1)) {
-                currentNum += 1;
-                currentStreak += 1;
-            }
+    const numSet = new Set(nums);
+        let longest = 0;
 
-            longestStreak = Math.max(longestStreak, currentStreak);
+        for (let num of numSet) {
+            if (!numSet.has(num - 1)) {
+                let length = 1;
+                while (numSet.has(num + length)) {
+                    length++;
+                }
+                longest = Math.max(longest, length);
+            }
         }
-    }
-    return longestStreak;
-};
+        return longest;
+}
