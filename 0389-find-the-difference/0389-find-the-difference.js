db4e2b15f07781f4d219d2311650af98c1ee5a2b
Mohammed Jisam <66182731+Mohammedjisam@users.noreply.github.com>
2025-01-23 10:34:06 +0530

    Time: 12 ms (12.79%), Space: 54 MB (6.67%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.js
+++ 0389-find-the-difference/0389-find-the-difference.js
@@ -4,13 +4,8 @@
  * @return {character}
  */
 var findTheDifference = function(s, t) {
- let sumS=0;
- let sumT=0;
- for(const char of s){
-    sumS+=char.charCodeAt(0);
- }
- for(const char of t){
-    sumT+=char.charCodeAt(0);
- }
- return String.fromCharCode(sumT-sumS)
+   for(let letter of s){
+    t=t.replace(letter,'');
+   }
+   return t
 };
