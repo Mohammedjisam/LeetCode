bf4cdad0943551f20b445b5083e508716ab8a670
Mohammed Jisam <66182731+Mohammedjisam@users.noreply.github.com>
2025-01-27 10:21:15 +0530

    Time: 0 ms (100%), Space: 49.3 MB (40.71%) - LeetHub

---
+++ 1341-split-a-string-in-balanced-strings/1341-split-a-string-in-balanced-strings.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var balancedStringSplit = function(s) {
+    let balance = 0; 
+    let count = 0; 
+    for (let char of s) {
+        if (char === 'L') {
+            balance++;
+        } else if (char === 'R') {
+            balance--;
+        }
+        if (balance === 0) {
+            count++;
+        }
+    }
+    return count;
+};
