5bc891c18d36d666a67ed4450b4b74b03e2ee99a
Mohammed Jisam <66182731+Mohammedjisam@users.noreply.github.com>
2025-02-09 23:10:31 +0530

    Time: 73 ms (48.19%), Space: 54.1 MB (21.69%) - LeetHub

---
+++ 3367-find-the-sum-of-encrypted-integers/3367-find-the-sum-of-encrypted-integers.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var sumOfEncryptedInt = function(nums) {
+    let sum = 0;
+
+    for (let num of nums) {
+        if (num < 10) {
+            sum += num;
+        } else {
+            let strNum = String(num);
+            let maxDigit = Math.max(...strNum); // Find the largest digit
+            let encrypted = Number(strNum.replace(/./g, maxDigit)); // Replace all digits with the largest digit
+            sum += encrypted;
+        }
+    }
+
+    return sum;
+};
