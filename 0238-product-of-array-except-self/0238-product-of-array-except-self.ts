062f02fc2b5c6333ec35dd721a1b6126ef3bfa47
Mohammed Jisam <66182731+Mohammedjisam@users.noreply.github.com>
2025-02-19 10:38:33 +0530

    Time: 3 ms (93.97%), Space: 67.8 MB (29.61%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.ts
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.ts
@@ -1,15 +1,13 @@
 function productExceptSelf(nums: number[]): number[] {
-    const n = nums.length;
-    const result: number[] = new Array(n).fill(1);
-    let leftProduct = 1;
-    for (let i = 0; i < n; i++) {
-        result[i] = leftProduct;
-        leftProduct *= nums[i];
+    if (nums.length < 2 || nums.length > 100000) throw new Error('Wrong array size');
+    let left = 1;
+    let right = 1;
+    const answer: number[] = Array(nums.length).fill(1);
+    for (let i = 0; i < nums.length; ++i) {
+        answer[i] *= left;
+        answer[nums.length - 1 - i] *= right;
+        left *= nums[i];
+        right *= nums[nums.length - 1 - i];
     }
-    let rightProduct = 1;
-    for (let i = n - 1; i >= 0; i--) {
-        result[i] *= rightProduct;
-        rightProduct *= nums[i];
-    }
-    return result;
+    return answer;
 }
