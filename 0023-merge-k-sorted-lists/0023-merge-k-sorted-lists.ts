3b248c287e1a36dbff667d3b750483eceff4d7dd
Mohammed Jisam <66182731+Mohammedjisam@users.noreply.github.com>
2025-02-28 23:49:05 +0530

    Time: 6 ms (89.45%), Space: 63.2 MB (17.3%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.ts
@@ -0,0 +1,30 @@
+function mergeKLists(lists: Array<ListNode | null>): ListNode | null {
+    if (lists.length === 0) return null;
+    function mergeTwoLists(l1: ListNode | null, l2: ListNode | null): ListNode | null {
+        let dummy = new ListNode(0);
+        let current = dummy;
+        while (l1 !== null && l2 !== null) {
+            if (l1.val < l2.val) {
+                current.next = l1;
+                l1 = l1.next;
+            } else {
+                current.next = l2;
+                l2 = l2.next;
+            }
+            current = current.next;
+        }
+        if (l1 !== null) current.next = l1;
+        if (l2 !== null) current.next = l2;
+        return dummy.next;
+    }
+    while (lists.length > 1) {
+        let mergedLists: Array<ListNode | null> = [];
+        for (let i = 0; i < lists.length; i += 2) {
+            const l1 = lists[i];
+            const l2 = i + 1 < lists.length ? lists[i + 1] : null;
+            mergedLists.push(mergeTwoLists(l1, l2));
+        }
+        lists = mergedLists;
+    }
+    return lists[0];
+}
