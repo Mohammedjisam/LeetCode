4eacc32edb54daa4f463b829e19cb0884cd56270
Mohammed Jisam <66182731+Mohammedjisam@users.noreply.github.com>
2025-02-28 18:17:15 +0530

    Time: 18 ms (94.83%), Space: 60.9 MB (11.55%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.ts
+++ 0076-minimum-window-substring/0076-minimum-window-substring.ts
@@ -1,19 +1,34 @@
-function minWindow(s, t) {
-    let map = {}, left = 0, right = 0, minStart = 0, minLen = Infinity, count = t.length;
-    for (let char of t) map[char] = (map[char] || 0) + 1;
-    while (right < s.length) {
-        if (map[s[right]] > 0) count--;
-        map[s[right]]--;
-        right++;
-        while (count === 0) {
-            if (right - left < minLen) {
-                minLen = right - left;
-                minStart = left;
-            }
-            map[s[left]]++;
-            if (map[s[left]] > 0) count++;
-            left++;
-        }
+function minWindow(s: string, t: string): string {
+    if (t.length === 0) return "";
+    let frequencies = new Map<string, number>();
+    for (let i = 0; i < t.length; i++) {
+        let targetEntry = frequencies.get(t[i]);
+        frequencies.set(t[i], targetEntry ? targetEntry + 1 : 1);
     }
-    return minLen === Infinity ? "" : s.substring(minStart, minStart + minLen);
-}
+    let have = 0;
+    let need = frequencies.size;
+    let left = 0;
+    let right = 0;
+    let resLength = Number.MAX_SAFE_INTEGER;
+    let resRange: number[] = [];
+    while (right < s.length) {
+        let entry = s[right];
+        if (frequencies.has(entry)) {
+            frequencies.set(entry, frequencies.get(entry)! - 1);
+            if (frequencies.get(entry) === 0) have++;
+        }
+        while (have == need) {
+            if (resLength > right - left + 1) {
+                resLength = right - left + 1;
+                resRange = [left, right];
+            }
+            let deleted = s[left++];
+            if (frequencies.has(deleted)) {
+                if (frequencies.get(deleted) === 0) have--;
+                frequencies.set(deleted, frequencies.get(deleted)! + 1);
+            }
+        }
+        right++;
+    }
+    return resLength === Number.MAX_SAFE_INTEGER ? "" : s.substring(resRange[0], resRange[1] + 1);
+};
