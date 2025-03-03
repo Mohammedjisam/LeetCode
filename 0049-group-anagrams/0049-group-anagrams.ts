1cd5a56cb65b4617a70789ab5e20d4a9c49ad82f
Mohammed Jisam <66182731+Mohammedjisam@users.noreply.github.com>
2025-03-03 11:52:14 +0530

    Time: 29 ms (74.3%), Space: 71.4 MB (5.22%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.ts
@@ -0,0 +1,11 @@
+function groupAnagrams(strs: string[]): string[][] {
+    const map = new Map<string, string[]>();
+    for (let str of strs) {
+        const sorted = str.split('').sort().join('');
+        if (!map.has(sorted)) {
+            map.set(sorted, []);
+        }
+        map.get(sorted)?.push(str);
+    }
+    return Array.from(map.values());
+}
