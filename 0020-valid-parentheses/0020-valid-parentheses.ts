bfec903a095b6c5181eceea81595d52d6282466c
Mohammed Jisam <66182731+Mohammedjisam@users.noreply.github.com>
2025-03-04 23:28:01 +0530

    Time: 2 ms (80.19%), Space: 58.6 MB (12.47%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.ts
@@ -0,0 +1,18 @@
+function isValid(s: string): boolean {
+  const stack: string[] = [];
+  const map: { [key: string]: string } = {
+    ')': '(',
+    '}': '{',
+    ']': '['
+  };
+  for (let char of s) {
+    if (char === '(' || char === '{' || char === '[') {
+      stack.push(char);
+    } else if (char in map) {
+      if (stack.length === 0 || stack.pop() !== map[char]) {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
+}
