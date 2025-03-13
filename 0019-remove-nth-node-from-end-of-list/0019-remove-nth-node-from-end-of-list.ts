89feeb1095f7357ee5b2a56ed76f6a33efa5c9c2
Mohammed Jisam <66182731+Mohammedjisam@users.noreply.github.com>
2025-03-13 12:17:35 +0530

    Time: 0 ms (100%), Space: 57.9 MB (13.83%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.ts
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.ts
@@ -1,17 +1,13 @@
 var removeNthFromEnd = function(head, n) {
     let res = new ListNode(0, head);
     let dummy = res;
-
     for (let i = 0; i < n; i++) {
         head = head.next;
     }
-
     while (head) {
         head = head.next;
         dummy = dummy.next;
     }
-
     dummy.next = dummy.next.next;
-
     return res.next;    
 };
