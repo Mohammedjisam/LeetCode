9a6c487c9990daa4b9a47300e9f60a0b3a75add4
Mohammed Jisam <66182731+Mohammedjisam@users.noreply.github.com>
2025-03-25 17:59:51 +0530

    Time: 54 ms (78.61%), Space: 57.8 MB (35.95%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.ts
+++ 0007-reverse-integer/0007-reverse-integer.ts
@@ -2,12 +2,10 @@ function reverse(x: number): number {
     let reversed = 0;
     const isNegative = x < 0;
     x = Math.abs(x);
-    
     while (x > 0) {
         const digit = x % 10;
         reversed = reversed * 10 + digit;
         x = Math.floor(x / 10);
-        
         if (reversed > Math.pow(2, 31) - 1) {
             return 0;
         }
