97db2e6731cd6a8938948cb1f7ec9a02f0571435
Mohammed Jisam <66182731+Mohammedjisam@users.noreply.github.com>
2025-04-02 01:32:38 +0530

    Time: 0 ms (100%), Space: 54.4 MB (61.81%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.ts
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.ts
@@ -1,24 +1,15 @@
 function swapPairs(head: ListNode | null): ListNode | null {
-    // If the list is empty or has only one node, return as is
     if (!head || !head.next) return head;
-
-    // Initialize a dummy node to simplify swapping
     let dummy = new ListNode(0, head);
     let prev = dummy;
-
     while (head && head.next) {
         let first = head;
         let second = head.next;
-
-        // Swapping nodes
         first.next = second.next;
         second.next = first;
         prev.next = second;
-
-        // Move pointers forward
         prev = first;
         head = first.next;
     }
-
     return dummy.next;
 }
