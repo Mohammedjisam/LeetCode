0266c3d44d81d781c20a5e8d2fb0a8ec57aded61
Mohammed Jisam <66182731+Mohammedjisam@users.noreply.github.com>
2025-04-02 01:34:33 +0530

    Time: 0 ms (100%), Space: 60.4 MB (51.35%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.ts
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.ts
@@ -1,9 +1,7 @@
 function reverseKGroup(head: ListNode | null, k: number): ListNode | null {
     if (!head || k <= 1) return head;
-
     let dummy = new ListNode(0, head);
     let prevGroupEnd = dummy;
-
     while (head) {
         let count = 0;
         let temp = head;
@@ -11,9 +9,7 @@ function reverseKGroup(head: ListNode | null, k: number): ListNode | null {
             temp = temp.next;
             count++;
         }
-
         if (count < k) break;
-
         let prev = null;
         let curr = head;
         for (let i = 0; i < k; i++) {
@@ -22,12 +18,10 @@ function reverseKGroup(head: ListNode | null, k: number): ListNode | null {
             prev = curr;
             curr = next;
         }
-
         prevGroupEnd.next = prev;
         head.next = curr;
         prevGroupEnd = head;
         head = curr;
     }
-
     return dummy.next;
 }
