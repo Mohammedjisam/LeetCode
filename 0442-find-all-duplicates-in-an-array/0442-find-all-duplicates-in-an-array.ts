1bbc449711446f44e6a22d6e1f6eb376f2446f9b
Mohammed Jisam <66182731+Mohammedjisam@users.noreply.github.com>
2025-04-07 10:06:01 +0530

    Time: 4 ms (81.73%), Space: 66.4 MB (63.46%) - LeetHub

---
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.ts
@@ -0,0 +1,14 @@
+function findDuplicates(nums: number[]): number[] {
+    const result: number[] = [];
+
+    for (let i = 0; i < nums.length; i++) {
+        const index = Math.abs(nums[i]) - 1;
+        if (nums[index] < 0) {
+            result.push(Math.abs(nums[i]));
+        } else {
+            nums[index] = -nums[index];
+        }
+    }
+
+    return result;
+}
