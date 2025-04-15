b830c5f9c50739396d545aea3f52db62bdd19d99
Mohammed Jisam <66182731+Mohammedjisam@users.noreply.github.com>
2025-04-15 09:36:56 +0530

    Time: 0 ms (100%), Space: 55.4 MB (64.08%) - LeetHub

--- 0068-text-justification/0068-text-justification.ts
+++ 0068-text-justification/0068-text-justification.ts
@@ -2,18 +2,15 @@ function fullJustify(words: string[], maxWidth: number): string[] {
     const result: string[] = [];
     let line: string[] = [];
     let lineLength = 0;
-
     for (let word of words) {
         if (lineLength + line.length + word.length > maxWidth) {
             let spacesToAdd = maxWidth - lineLength;
             let lineStr = "";
-
             if (line.length === 1) {
                 lineStr = line[0] + " ".repeat(spacesToAdd);
             } else {
                 const spaceBetween = Math.floor(spacesToAdd / (line.length - 1));
                 const extraSpaces = spacesToAdd % (line.length - 1);
-
                 for (let i = 0; i < line.length; i++) {
                     lineStr += line[i];
                     if (i < line.length - 1) {
@@ -21,17 +18,13 @@ function fullJustify(words: string[], maxWidth: number): string[] {
                     }
                 }
             }
-
             result.push(lineStr);
             line = [];
             lineLength = 0;
         }
-
         line.push(word);
         lineLength += word.length;
     }
-
-    // Last line (left-justified)
     const lastLine = line.join(" ") + " ".repeat(maxWidth - (line.join(" ").length));
     result.push(lastLine);
 
