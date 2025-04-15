04c995849625a5cd85f2df0159de47354cc93688
Mohammed Jisam <66182731+Mohammedjisam@users.noreply.github.com>
2025-04-15 09:42:03 +0530

    Time: 0 ms (100%), Space: 60.4 MB (82.33%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.ts
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.ts
@@ -1,11 +1,9 @@
 function findMaxConsecutiveOnes(nums: number[]): number {
     let maxCount: number = 0;
     let currentCount: number = 0;
-
     for (let i = 0; i < nums.length; i++) {
         currentCount = nums[i] === 1 ? currentCount + 1 : 0;
         maxCount = Math.max(maxCount, currentCount);
     }
-
     return maxCount;
 }
