cfd9d54158a1040a238b9f269eb3b3123619e4fc
Mohammed Jisam <66182731+Mohammedjisam@users.noreply.github.com>
2025-04-27 23:33:30 +0530

    Time: 0 ms (100%), Space: 55.2 MB (6.73%) - LeetHub

---
+++ 0832-binary-tree-pruning/0832-binary-tree-pruning.js
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var pruneTree = (root) => {
+    if (root.left)  root.left  = pruneTree(root.left);
+    if (root.right) root.right = pruneTree(root.right);
+    if (!root.left && !root.right && root.val === 0) return null;
+    return root
+};
